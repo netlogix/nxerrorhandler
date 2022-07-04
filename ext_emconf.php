1f7b03f028c3448ff0249cf54f5b971e61269b82
Alexander Jahn <alexander.jahn@netlogix.de>
2022-07-04 09:46:49 +0200

    BREAKING: remove TYPO3 9.5 compatibility
    This removes compatibility with TYPO3 9.5.

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.4.0-8.1.0',
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
