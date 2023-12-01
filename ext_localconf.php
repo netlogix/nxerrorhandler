6995a181644cdbd99ba19991816b9174d2dc150a
Tim Weisenberger <tim.weisenberger@netlogix.de>
2023-12-01 16:33:13 +0100

    refactor: Remove obsolete FatalErrorHandler

--- ext_localconf.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('TYPO3') or die();
-
-call_user_func(function () {
-    \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \Netlogix\Nxerrorhandler\ErrorHandler\FatalErrorHandler::class
-    )->initialize();
-});
