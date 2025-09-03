3b0106aec96f403612df4dfca36a52aeb22e9e7e
Sascha Nowak <sascha.nowak@netlogix.de>
2025-09-03 07:47:11 +0200

    fix: handle early exception correctly

--- Classes/Controller/ErrorPageController.php
+++ Classes/Controller/ErrorPageController.php
@@ -7,6 +7,7 @@ namespace Netlogix\Nxerrorhandler\Controller;
 use Netlogix\Nxerrorhandler\Service\StaticDocumentOutputService;
 use Psr\Http\Message\ServerRequestInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+use TYPO3\CMS\Core\Http\ServerRequestFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 #[Autoconfigure(public: true)]
@@ -18,11 +19,15 @@ readonly class ErrorPageController extends \TYPO3\CMS\Core\Controller\ErrorPageC
         int $errorCode = 0,
         ?int $httpStatusCode = null,
     ): string {
-        $output = GeneralUtility::makeInstance(StaticDocumentOutputService::class)->getOutput(
-            $httpStatusCode ?? 500,
-            $this->getRequest(),
-            $message,
-        );
+        try {
+            $output = GeneralUtility::makeInstance(StaticDocumentOutputService::class)->getOutput(
+                $httpStatusCode ?? 500,
+                $this->getRequest(),
+                $message,
+            );
+        } catch (\Throwable) {
+            $output = '';
+        }
 
         if ($output === '') {
             return parent::errorAction($title, $message, $errorCode, $httpStatusCode);
@@ -33,6 +38,9 @@ readonly class ErrorPageController extends \TYPO3\CMS\Core\Controller\ErrorPageC
 
     private function getRequest(): ServerRequestInterface
     {
-        return $GLOBALS['TYPO3_REQUEST'];
+        if (array_key_exists('TYPO3_REQUEST', $GLOBALS)) {
+            return $GLOBALS['TYPO3_REQUEST'];
+        }
+        return ServerRequestFactory::fromGlobals();
     }
 }
