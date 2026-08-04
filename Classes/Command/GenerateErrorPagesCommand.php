494743b4cc6d55c27dfce6484241bbd9f12dffd8
Tim Weisenberger <tim.weisenberger@netlogix.de>
2026-08-04 09:01:22 +0200

    fix: Avoid CLI-hostile ServerRequestFactory::fromGlobals() in error page generation
    TYPO3 v14 throws InvalidRequestUrlOnCliException when fromGlobals() cannot
    derive a request URL from $_SERVER on CLI. The generated URI was immediately
    overwritten with the site base anyway, so build the ServerRequest directly.

--- Classes/Command/GenerateErrorPagesCommand.php
+++ Classes/Command/GenerateErrorPagesCommand.php
@@ -17,7 +17,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Exception\SiteNotFoundException;
-use TYPO3\CMS\Core\Http\ServerRequestFactory;
+use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\LinkHandling\LinkService;
 use TYPO3\CMS\Core\Site\Entity\Site;
 use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
@@ -92,8 +92,7 @@ class GenerateErrorPagesCommand extends Command implements LoggerAwareInterface
                         continue;
                     }
 
-                    $request = ServerRequestFactory::fromGlobals()
-                        ->withUri($site->getBase())
+                    $request = (new ServerRequest($site->getBase()))
                         ->withAttribute('site', $site)
                         ->withAttribute('language', $language);
 
